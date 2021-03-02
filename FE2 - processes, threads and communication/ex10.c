a0d5caee3f7b1ca74ec76f35670cbf13887d64d5
Margarida Vieira <margaridajcv.27@gmail.com>
2021-03-02 21:49:33 +0000

    ex10

--- FE2 - processes, threads and communication/ex10.c
+++ FE2 - processes, threads and communication/ex10.c
@@ -6,14 +6,19 @@
 
 #define NTHREADS 2
 
-void *func(void *a) {
-    switch(*((int*) a)) {
-        case 0:
-            printf("Operating ");
-            break;
-        case 1:
-            printf("Systems\n");
-            break;
+struct Argument_t {
+    int id;
+    char* seq1;
+    char* seq2;
+};
+
+void *executer(void *a) {
+    if (((struct Argument_t*) a)->id == 0) {
+        printf("%s", ((struct Argument_t*) a)->seq1);
+        ((struct Argument_t*) a)->id = 1;
+    }
+    else if (((struct Argument_t*) a)->id == 1) {
+        printf("%s", ((struct Argument_t*) a)->seq2);
     }
     
 	pthread_exit(NULL);	
@@ -23,11 +28,20 @@ int main() {
 	int i;	
 	pthread_t ids[NTHREADS]; 
 
+    struct Argument_t *arg = malloc(sizeof(struct Argument_t));
+    arg->id = 0;
+    arg->seq1 = "Operating ";
+    arg->seq2 = "Systems\n";
+
 	for(i = 0; i < NTHREADS; i++) {
         int *val = malloc(sizeof(int));
         *val = i;
 
-		if (pthread_create(ids + i, NULL, func, (void*) val) != 0) {
+        if (i == 1) {
+            sleep(0.2); //otherwise, both threads would most likely print the same thing
+        }
+
+		if (pthread_create(ids + i, NULL, executer, (void*) arg) != 0) {
             perror("Failed to create thread.\n");
 			exit(-1); 
         }
