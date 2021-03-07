283dd080f86faa6c839e18e8a340cef961954f27
Margarida Vieira <margaridajcv.27@gmail.com>
2021-03-07 10:48:29 +0000

    change in ex13c

--- FE2 - processes, threads and communication/ex13c.c
+++ FE2 - processes, threads and communication/ex13c.c
@@ -6,10 +6,11 @@
 #include <sys/wait.h>
 
 int main() {
-    int pp[2];
+    int pp1[2];
+    int pp2[2];
     int pid;
 
-    if (pipe(pp) == -1) { 
+    if (pipe(pp1) == -1 || pipe(pp2) == -1) { 
         perror("Failed to create pipe.\n");
         exit(1); 
     }
@@ -22,25 +23,27 @@ int main() {
     if (pid == 0) { //child
         char msg_pt1[1024];
 
-        read(pp[0], msg_pt1, sizeof(msg_pt1));
-        close(pp[0]);
+        close(pp1[1]);
+        read(pp1[0], msg_pt1, sizeof(msg_pt1));
+        close(pp1[0]);
 
         char *msg_pt2 = "Systems";
         char *msg_final = strcat(msg_pt1, msg_pt2);
 
-        write(pp[1], msg_final, 1+strlen(msg_final));
-        close(pp[1]);
+        close(pp2[0]);
+        write(pp2[1], msg_final, 1+strlen(msg_final));
+        close(pp2[1]);
     }
     else { //parent
-        write(pp[1], "Operating ", 1+strlen("Operating "));
-        close(pp[1]);
-
-        wait(NULL); // waiting for the child to finish
+        close(pp1[0]);
+        write(pp1[1], "Operating ", 1+strlen("Operating "));
+        close(pp1[1]);
 
         char msg[1024];
 
-        read(pp[0], msg, sizeof(msg));
-        close(pp[0]);
+        close(pp2[1]);
+        read(pp2[0], msg, sizeof(msg));
+        close(pp2[0]);
 
         printf("%s\n", msg);
     }	
