9235b9255b59aa853e1836ea0396acdaea2cd5c6
Margarida Vieira <margaridajcv.27@gmail.com>
2021-03-16 17:47:03 +0000

    ex11 still not implemented

--- FE3 - processes, threads and coordination/ex7b.c
+++ FE3 - processes, threads and coordination/ex7b.c
@@ -1,7 +1,3 @@
-/* Testing access issues to shared variables
- * - Race conditions!
- * JMMC - Out.2001
- */
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -10,7 +6,11 @@
 long count;
 void *thr_fun(void *);
 
+pthread_mutex_t mutex;
+
 int main(int argc, char *argv[]) {
+    pthread_mutex_init(&mutex, NULL);
+
 	pthread_t *ptid;
 	int nthreads;
 	int i;
@@ -20,33 +20,42 @@ int main(int argc, char *argv[]) {
 		printf("\nUsage: %s #threads\n", argv[0]);
 		exit(1);
 	}
-
+	
 	nthreads = atoi(argv[1]);
 	ptid = (pthread_t *)malloc(nthreads * sizeof(pthread_t));
 	if (ptid == NULL) {
 		perror("malloc");
 		exit(2);
 	}
-
 	for (i = 0; i < nthreads; i++) {
 		if (pthread_create(&ptid[i], NULL, thr_fun, NULL) != 0) {
 			perror("pthread_create");
 			exit(3);
 		}
 	}
-	
+
 	for (i = 0; i < nthreads; i++)
 		pthread_join(ptid[i], NULL);
 
 	printf("\nEND!\n");
+
+	pthread_mutex_destroy(&mutex);
 	return 0;
 }
 
 void *thr_fun(void *arg) {
 	while (count < MAXCOUNT) {
-		printf("\n%ld  ->  printed by thread %lu\n", count, (unsigned long)pthread_self());
-		count++;
+		pthread_mutex_lock(&mutex);
+
+		if (count < MAXCOUNT) {
+			printf("\n%ld  ->  printed by thread %lu\n", count, (unsigned long)pthread_self());
+			count++;
+		}
+
+		pthread_mutex_unlock(&mutex);
 	}
+
 	printf("\nEND thread %lu!\n", (unsigned long)pthread_self());
+	
 	return (NULL);
 }
